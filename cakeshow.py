572cd0114fb81bda0f4a5196ff77d019282162ee
winterdraws <jbecks@kcai.edu>
2020-10-19 22:09:24 +0000

    Support for real stat matchups, requirements

--- cakeshow.py
+++ cakeshow.py
@@ -14,6 +14,22 @@ class Gameshow():
 
 pending_show = None
 
+def get_lucky_text():
+  random_lucky_text.append('The cake **sparkles**!! :sparkles:')
+  random_lucky_text.append('The cake **gleams**!! :sparkles:')
+  random_lucky_text.append('The cake **glows**!! :sparkles:')
+  random_lucky_text.append('A **choir of angels** sings!! :sparkles:')
+  return random.choice(random_lucky_text)
+
+def get_unlucky_text():
+  random_unlucky_text = []
+  random_unlucky_text.append('The cake **is badly burned**!!')
+  random_unlucky_text.append('The cake **screeches, bloodcurdlingly**!!')
+  random_unlucky_text.append('The cake **levitates ominously**!!')
+  random_unlucky_text.append('The cake **begins to weep blood!**')
+  random_unlucky_text.append('The cake **bursts into flames**!!')
+  return random.choice(random_unlucky_text)
+
 async def broadcast(message):
     await botuser.botuser.broadcast(message)
 
@@ -33,12 +49,43 @@ async def run_show():
     if pending_show is not None:
         p1 = pending_show.participant1
         p2 = pending_show.participant2
-
-        winner = random.choice([p1, p2])
-        im_winner = imaging.open_image_path(winner.get_portrait_path())
+        p1_quality = 0
+        p2_quality = 0
 
         desc = "***" + p1.get_sound() + "*** " + p1.name + " " + p1.get_verb().lower() + " a " + p1.get_cake_adjective().lower() + " " + p1.get_cake_flavor().lower() + " " + p1.get_cake_type().lower() + "!!"
+
+        p1_quality = p1.get_roll()
+
+        if (p1.get_unlucky()):
+          desc += ' ' + get_unlucky_text()
+          p1_quality = 1
+
+        if (p1.get_lucky()):
+          desc += ' ' + get_lucky_text()
+          p1_quality += 30
+
         desc += "\n***" + p2.get_sound() + "*** " + p2.name + " " + p2.get_verb().lower() + " a " + p2.get_cake_adjective().lower() + " " + p2.get_cake_flavor().lower() + " " + p2.get_cake_type().lower() + "!!"
+
+        p2_quality = p2.get_roll()
+
+        if (p2.get_unlucky()):
+          desc += ' ' + get_unlucky_text()
+          p2_quality = 1
+
+        if (p2.get_lucky()):
+          desc += ' ' + get_lucky_text()
+          p2_quality += 30
+
+        if (p1_quality == p2_quality):
+          winner = random.choice([p1, p2])
+        else:
+          if (p1_quality > p2_quality):
+            winner = p1
+          else:
+            winner = p2
+        
+        im_winner = imaging.open_image_path(winner.get_portrait_path())
+
         desc += "\n\n*" + winner.name + "* has swayed the judges with " + winner.get_pronoun('their') + " skill! ***VICTORY! :sparkles:***"
 
         new_embed = discord.Embed(title="THE WINNER IS...",
@@ -75,7 +122,7 @@ async def prep_show():
 
         res = imaging.concatenate(im1, im2)
 
-        new_embed = discord.Embed(title="UP NEXT...", description="The following contestants will bake next. The show will start shortly.", color=0xffd300)
+        new_embed = discord.Embed(title="UP NEXT...", description="The following contestants will bake next. The winner will be announced in 30 minutes.\nUse `!cheer [contestant]` to cheer for the contestant you think will win.", color=0xffd300)
         new_embed.add_field(name="Contestant 1", value=n1, inline=True)
         new_embed.add_field(name="Contestant 2", value=n2, inline=True)
         file = imaging.get_image_file(res)
--- periodic.py
+++ periodic.py
@@ -5,21 +5,24 @@ import cakeshow
 
 class Periodic:
     def __init__(self):
-        self.next_event = arrow.utcnow().replace(hour=17, minute=10).shift(days=+1)
+        self.next_event = arrow.utcnow().shift(minutes=+1)
         self.schedule_next_event()
 
     def schedule_next_event(self):
+      self.next_event = arrow.utcnow().shift(minutes=+1)
+        """
         if arrow.utcnow().hour < 17:
-            self.next_event = arrow.utcnow().replace(hour=17, minute=00)
+            self.next_event = arrow.utcnow().replace(hour=22, minute=09)
         else:
-            self.next_event = arrow.utcnow().replace(hour=17, minute=00).shift(days=+1)
+            self.next_event = arrow.utcnow().replace(hour=22, minute=00).shift(days=+1)
+        """"
 
     async def update(self):
         if self.next_event <= arrow.utcnow():
             self.schedule_next_event()
 
             try:
-                Botuser.broadcast("It's the reset!")
+                cakeshow.test_show()
             except Exception as e:
                 print('>> ENCOUNTERED AN EXCEPTION IN RUN_SCHEDULED')
                 print(e)
