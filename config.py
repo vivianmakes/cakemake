6f911085e93ebe5bc88f4fbc1f3f2f589f9c9e37
winterdraws <jbecks@kcai.edu>
2020-10-19 22:42:30 +0000

    channel now fetched as int

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Credentials():
   def __init__(self):
     self.bot_token = os.getenv('BOT_TOKEN')
-    self.broadcast_channel = os.getenv('BROADCAST_CHANNEL')
+    self.broadcast_channel = int(os.getenv('BROADCAST_CHANNEL'))
 
 credentials = Credentials()
