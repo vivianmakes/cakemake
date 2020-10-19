949853dabfe49a0ff7d49d0eba4ed3719dd02f17
winterdraws <jbecks@kcai.edu>
2020-10-19 22:23:28 +0000

    I literally. am so bad at this without an environment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import yaml
 import os
 
 class Credentials():
-  def __init__():
+  def __init__(self):
     self.bot_token = os.getenv('BOT_TOKEN')
     self.broadcast_channel = os.getenv('BROADCAST_CHANNEL')
 
